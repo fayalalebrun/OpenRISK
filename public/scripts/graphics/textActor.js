6fbf1ef1fcac89e040fd77a51523c2a82c5ceccf
Francisco Ayala Le Brun <frankxlebrun@gmail.com>
2020-01-01 20:54:57 +0100

    Add TextActor class

---
+++ public/scripts/graphics/textActor.js
@@ -0,0 +1,30 @@
+/**
+ * @fileOverview Contains TextActor class.
+ * @name textActor.js
+ * @author Francisco Ayala Le Brun <frankxlebrun@gmail.com>
+ */
+
+import {Actor} from './actor.js';
+
+export class TextActor extends Actor {
+    constructor(parent, x, y, z, rotation, scale, text, fillStyle = 'black', font='10px sans-serif', textAlign='start',
+		textBaseline='alphabetic',direction='inherit'){
+	super(parent, x, y, z, 0, 0, rotation, scale);
+	this.text = text;
+	this.fillStyle = fillStyle;
+	this.font = font;
+	this.textAlign = textAlign;
+	this.textBaseline = textBaseline;
+	this.direction = direction;
+    }
+
+    do_draw (ctx) {
+	ctx.fillStyle = this.fillStyle;
+	ctx.font = this.font;
+	ctx.textAlign = this.textAlign;
+	ctx.textBaseline = this.textBaseline;
+	ctx.direction = this.direction;
+	ctx.fillText(this.text, this.x, this.y);
+
+    }
+};
